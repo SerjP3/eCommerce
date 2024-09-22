3e3ca6ee9bb7fc3132b113ae004da7d84abfcc40
sprodan <sprodan@deloittece.com>
2024-09-22 15:27:04 +0200

    Revert "First commit"
    This reverts commit 4b1d6078485bb5ef29ef16fe397a21586b5d8dd9.

--- src/main/java/com/moonlight/shopping_cart/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.moonlight.shopping_cart.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
